c60c987bbfb811eb1aba65bc3332df9ea92906a6
Maadelka <92347900+Maadelka@users.noreply.github.com>
2022-01-25 22:31:51 +0700

    Update script.js
    merapikan js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,7 @@
 
-
-
-
-
 const dropd=document.getElementById('dropd')
 
 const burger=document.getElementById('burger')
 burger.addEventListener('click',function(){
     dropd.classList.toggle('muncul')
 })
-
-
-// const mDropI=document.getElementById('mDropI')
-
-// const mDrop=document.getElementById('mDrop')
-// mDrop.addEventListener('click',function(){
-//     mDropI.classList.toggle('mDrop')
-// })
