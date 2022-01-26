2d6b3e74a448c632b3d2ade7a1d3649f7ea99e17
Maadelka <92347900+Maadelka@users.noreply.github.com>
2022-01-26 12:31:26 +0700

    Update script.js
    add js burger rotate

--- js/script.js
+++ js/script.js
@@ -4,4 +4,5 @@ const dropd=document.getElementById('dropd')
 const burger=document.getElementById('burger')
 burger.addEventListener('click',function(){
     dropd.classList.toggle('muncul')
+    burger.classList.toggle('putar')
 })
